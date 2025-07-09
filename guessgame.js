d63884777a9f8b6bfbf7dc74f2d3d8c794c269bc
karthick <karthickfaf19.7@gmail.com>
2025-07-09 18:27:38 +0530

    feat : created guessing game using javascript

---
+++ guessgame.js
@@ -0,0 +1,38 @@
+
+
+<h1>guess game </h1>
+<p>guess number form 1 to 10</p>
+
+<input id="inp" type="number">
+<button onclick="che()">check</button>
+
+<p id="res"></p>
+<p id="rel"></p>
+
+<script>
+    
+
+    var inp1 = document.getElementById("inp");
+    var res = document.getElementById("res");
+    var res1 = document.getElementById("rel");
+
+    var ran1 = Math.random();
+    var ran = Math.floor((ran1*10)+1);
+
+
+    var num = inp1.value
+
+    function che(){
+
+        if(num == ran){
+            console.log(res.textContent = "you won")
+            alert("you won buddy")
+        }
+        else{
+            console.log(res.textContent = "you lose")
+            alert("you lose buddy")
+        }
+
+        console.log(res1.textContent = ran);
+    }
+</script>
